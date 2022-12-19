bc5c41244b88cfa9629a0c523da41cf0d06045ce
kirillbowkin <kirillshishkin77@gmail.com>
2022-12-19 03:16:34 +0300

    8. Обновление модели с помощью события EntityChangedEvent

--- src/main/java/com/sample/hotel/listener/BookingEventListener.java
+++ src/main/java/com/sample/hotel/listener/BookingEventListener.java
@@ -1,7 +1,13 @@
 package com.sample.hotel.listener;
 
 import com.sample.hotel.entity.Booking;
+import com.sample.hotel.entity.BookingStatus;
+import com.sample.hotel.entity.RoomReservation;
+import io.jmix.core.DataManager;
+import io.jmix.core.Id;
+import io.jmix.core.event.EntityChangedEvent;
 import io.jmix.core.event.EntitySavingEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +17,28 @@ import java.time.temporal.ChronoUnit;
 @Component
 public class BookingEventListener {
 
+    @Autowired
+    private DataManager dataManager;
+
     @EventListener
     public void onBookingSaving(EntitySavingEvent<Booking> event) {
         Booking savingBooking = event.getEntity();
         LocalDate arrivalDate = savingBooking.getArrivalDate();
         Integer nightsOfStay = savingBooking.getNightsOfStay();
         savingBooking.setDepartureDate(arrivalDate.plus(nightsOfStay, ChronoUnit.DAYS));
+    }
+
+    @EventListener
+    public void onBookingChangedBeforeCommit(EntityChangedEvent<Booking> event) {
+        if (event.getChanges().isChanged("status")) {
+            Id<Booking> entityId = event.getEntityId();
+            Booking booking = dataManager.load(entityId).optional().orElse(null);
+
+            if(booking.getStatus().equals(BookingStatus.CANCELLED)) {
+                RoomReservation roomReservation = booking.getRoomReservation();
+                dataManager.remove(roomReservation);
+            }
+        }
+
     }
 }
